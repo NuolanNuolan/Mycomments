e36055f6cd844ba9f05886070fc651996ec15412
Bruce <bruce@nantekiMacBook-Pro.local>
2015-07-03 15:50:17 +0800

    init page

---
+++ MyComments/HomeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeTableViewController.h
+//  MyComments
+//
+//  Created by Bruce on 15-7-2.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeTableViewController : UITableViewController
+
+@end
---
+++ MyComments/SearchTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchTableViewController.h
+//  MyComments
+//
+//  Created by Bruce on 15-7-2.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchTableViewController : UITableViewController
+
+@end
